295f23fc0fdb6601a10d869487855a4c842f384a
Frank Lin <flinhong@hotmail.com>
2018-04-06 02:01:56 +0800

    test npm deploy

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+const htmlmin = require('gulp-htmlmin');
+
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('./**/*.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8',
+      level: 2
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('minify-js', function (callback) {
+  pump([
+        gulp.src(['./**/*.js']),
+        uglify(),
+        gulp.dest('./')
+    ],
+    callback
+  );
+});
+
+gulp.task('minify-html', function() {
+  return gulp.src('./**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./'));
+});
